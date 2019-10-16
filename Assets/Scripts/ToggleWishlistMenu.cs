68251987affeb4a9e201fe975f3102f60be477ec
CHARLTONPC\Charlton <charlton.lane14@gmail.com>
2019-10-17 08:39:02 +1100

    Added travis branch to master

---
+++ Assets/Scripts/ToggleWishlistMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleWishlistMenu : MonoBehaviour {
+
+    // THIS CLASS IS UNUSED ATM.
+
+
+    private bool isActive = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void EnableWishlistMenu() {
+        if (!gameObject.activeSelf) {
+            gameObject.SetActive(true);
+            isActive = true;
+        }
+    }
+
+    public void DisableWishlistMenu() {
+        if (gameObject.activeSelf) {
+            gameObject.SetActive(false);
+            isActive = false;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/TourGuideLogic.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TourGuideLogic : MonoBehaviour
+{
+
+    public GameObject[] artworks = new GameObject[11];
+    public bool onTour = false;
+    public int artworkIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = artworks[artworkIndex].transform.GetChild(3).transform.position;
+    }
+
+    public void moveToNextArtwork()
+    {
+        artworkIndex++;
+        if (artworkIndex >= artworks.Length)
+        {
+            artworkIndex = 0;
+        }
+       
+    }
+
+    public void moveToPrevArtwork()
+    {
+        artworkIndex--;
+        if (artworkIndex < 0)
+        {
+            artworkIndex = artworks.Length-1;
+        }
+      
+    }
+    
+
+    public void cancelTour()
+    {
+
+    }
+
+    public void pauseTour()
+    {
+
+    }
+}
