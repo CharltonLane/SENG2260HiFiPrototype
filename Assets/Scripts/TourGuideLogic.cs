face8b82c8f71b0933e79614cb77ac88098477d0
Travis <56100117+t-rav4@users.noreply.github.com>
2019-10-16 17:29:46 +1100

    First commit

---
+++ Assets/Scripts/TourGuideLogic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TourGuideLogic : MonoBehaviour
+{
+
+    public GameObject[] artworks = new GameObject[11];
+    public bool onTour = false;
+    public int artworkIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (onTour)
+        {
+            
+        }
+    }
+
+    public void moveToNextArtwork()
+    {
+        gameObject.transform.position = artworks[artworkIndex+1].transform.GetChild(3).transform.position;
+    }
+
+    public void moveToPrevArtwork()
+    {
+
+    }
+    
+
+    public void cancelTour()
+    {
+
+    }
+
+    public void pauseTour()
+    {
+
+    }
+}
