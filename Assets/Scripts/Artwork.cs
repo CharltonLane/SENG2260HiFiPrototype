f69afe739e3243f8a78bda27a9761cf4173ab84e
CHARLTONPC\Charlton <charlton.lane14@gmail.com>
2019-10-25 17:38:10 +1100

    Info display shows non sample text
    The display next to the artwork now shows whatever title, author, description and price is set in the inspector

--- Assets/Scripts/Artwork.cs
+++ Assets/Scripts/Artwork.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Artwork : MonoBehaviour {
 
@@ -13,13 +14,23 @@ public class Artwork : MonoBehaviour {
 
 	public bool hasBeenLiked = false;
 
+	public GameObject infoPanel;
+
 	// Start is called before the first frame update
 	void Start()
     {
 		if (transform.childCount != 0) { // This is a dodgy workaround, if it has children it is a physical artwork in the gallery, if not it's a UI button in the wishlist.
 			sprite = transform.GetChild(0).GetComponent<SpriteRenderer>().sprite;
+
+			// Now update the info panel text.
+			infoPanel.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = title;
+			infoPanel.transform.GetChild(1).GetComponent<TMPro.TextMeshProUGUI>().text = author;
+			infoPanel.transform.GetChild(2).GetComponent<TMPro.TextMeshProUGUI>().text = description;
+			infoPanel.transform.GetChild(3).GetComponent<TMPro.TextMeshProUGUI>().text = price;
 		}
-    }
+
+		
+	}
 
     // Update is called once per frame
     void Update()
